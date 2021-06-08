07a3c53934c14e6b13fbf459bc1854f3dcc6dead
IgrGabriel <70847049+IgrGabriel@users.noreply.github.com>
2021-06-07 22:53:18 -0300

    Update interior.hs

--- Functional Programming/Haskell/03_Aquecimento/interior.hs
+++ Functional Programming/Haskell/03_Aquecimento/interior.hs
@@ -1,9 +1,1 @@
 interior xs = take ((length xs) - 2) (drop 1 xs)
-
-{-
-interior [1,2] == []
-interior [1,3,2] == [3]
-interior [2,5,3,7,3] == [5,3,7]
-interior [2,2,2,4] == [2,2]
-interior [1,2,3,5,7,8] == [2,3,5,7]
--}
