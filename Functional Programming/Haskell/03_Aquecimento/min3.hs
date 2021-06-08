baac09657650f281fb0a463e29a9c5184b5d85f4
IgrGabriel <70847049+IgrGabriel@users.noreply.github.com>
2021-06-07 22:53:54 -0300

    Update min3.hs

--- Functional Programming/Haskell/03_Aquecimento/min3.hs
+++ Functional Programming/Haskell/03_Aquecimento/min3.hs
@@ -3,13 +3,3 @@ min3 a b c
     | b < a && a <= c = b
     | c < a && a <= b = c
     | otherwise = a
-    
-{- 
-	--IN : Três números, x, y e z
-	--OUT: Menor valor entre x, y e z
-	
-	min3 1 2 3 == 1
-	min3 2 1 3 == 1
-	min3 3 4 2 == 2
-	min3 2 5 4 == 2
--}
